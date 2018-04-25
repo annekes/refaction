8fd192b889c853c31d8dd01b73e357df3524b093
annekes <annekesmitheram@gmail.com>
2018-04-26 10:55:23 +1200

    Split out Products.cs so that each class had its own file: Product, ProductOption and ProductOptions.

---
+++ refactor-me/Models/ProductOptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Web;
+
+namespace refactor_me.Models
+{
+    public class ProductOptions
+    {
+        public List<ProductOption> Items { get; private set; }
+
+        public ProductOptions()
+        {
+            LoadProductOptions(null);
+        }
+
+        public ProductOptions(Guid productId)
+        {
+            LoadProductOptions(productId.ToString());
+        }
+
+        private void LoadProductOptions(string productId)
+        {
+            Items = new List<ProductOption>();
+            var conn = Helpers.NewConnection();
+            var where = $"where productid = '{productId}'";
+            var cmd = new SqlCommand($"select id from productoption {where}", conn);
+            conn.Open();
+
+            var rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var id = Guid.Parse(rdr["id"].ToString());
+                Items.Add(new ProductOption(id));
+            }
+        }
+    }
+}
